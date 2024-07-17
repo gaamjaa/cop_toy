cb1c4c2031b94c8184b466a051c2004242926e54
gaamjaa <dlgodus21@gmail.com>
2024-07-17 22:14:53 +0900

    [update] page change function

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import os
 
+# from dotenv import load_dotenv
+# load_dotenv()
+
 # os.environ["OPENAI_API_KEY"] = os.getenv("openai_api_key")
 os.environ["OPENAI_API_KEY"] = st.secrets["openai_api_key"]
+
 from langchain_openai import ChatOpenAI
 model = ChatOpenAI(model="gpt-3.5-turbo")
 
@@ -15,38 +19,53 @@ st.set_page_config(page_title="main", page_icon="💯")
 if 'selected_type' not in st.session_state:
     st.session_state.selected_type = 0
 
-st.title("문제 만들기 :memo::100:")
-st.divider()
-st.subheader("원하는 문제 스타일을 선택해주세요.")
+if 'current_page' not in st.session_state:
+    st.session_state.current_page = "main"
 
-st.columns(3)
-col1, col2, col3 = st.columns(3)
+def changepage(name): st.session_state.current_page = name
 
-with col1:
-    if st.button("객관식"):
-        st.session_state.selected_type = 1
-with col2:
-    if st.button("주관식"):
-        st.session_state.selected_type = 2
-with col3:
-    if st.button("서술형"):
-        st.session_state.selected_type = 3
+placeholder = st.empty()
+
+
+if st.session_state.current_page == "main":
+    print("main")
     
-if st.session_state.selected_type == 1:
-    options = st.multiselect(
-            "객관식의 유형을 선택해주세요 (선택)",
-            ["하나만 고르기", "여러개 고르기"],
-            ["하나만 고르기", "여러개 고르기"])
-elif st.session_state.selected_type == 2:
-    st.write("주관식 선택")
-elif st.session_state.selected_type == 3:
-    st.write("서술형 선택")
+    st.title("문제 만들기 :memo::100:")
+    st.divider()
+    st.subheader("원하는 문제 스타일을 선택해주세요.")
 
-uploaded_file = st.file_uploader("문제를 낼 학습자료를 업로드 해주세요")
-if uploaded_file is not None:
-    st.write("uploaded!")
-    
-if st.button("문제 생성하기"):
+    st.columns(3)
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        if st.button("객관식"):
+            st.session_state.selected_type = 1
+    with col2:
+        if st.button("주관식"):
+            st.session_state.selected_type = 2
+    with col3:
+        if st.button("서술형"):
+            st.session_state.selected_type = 3
+        
+    if st.session_state.selected_type == 1:
+        options = st.multiselect(
+                "객관식의 유형을 선택해주세요 (선택)",
+                ["하나만 고르기", "여러개 고르기"],
+                ["하나만 고르기", "여러개 고르기"])
+    elif st.session_state.selected_type == 2:
+        st.write("주관식 선택")
+    elif st.session_state.selected_type == 3:
+        st.write("서술형 선택")
+
+    uploaded_file = st.file_uploader("문제를 낼 학습자료를 업로드 해주세요")
+    if uploaded_file is not None:
+        st.write("uploaded!")
+        
+    st.button("문제 생성하기", on_click=lambda :changepage("problem"))
+
+elif st.session_state.current_page == "problem":
+    print("problem")
+    st.title("문제 생성 페이지")
     prompt = ChatPromptTemplate.from_template("tell me a short joke about {topic}")
     output_parser = StrOutputParser()
 
@@ -56,21 +75,54 @@ if st.button("문제 생성하기"):
     print(prompt_value)
     st.write("hello world!")
     st.write(prompt_value)
+    
+    st.button("돌아가기", on_click=lambda :changepage("main"))
+    
+    
+    
+    
+    
+    
+    
+#######################################################################    
+# st.title("문제 만들기 :memo::100:")
+# st.divider()
+# st.subheader("원하는 문제 스타일을 선택해주세요.")
 
+# st.columns(3)
+# col1, col2, col3 = st.columns(3)
 
-# 이렇게 만들어서 제출하기 누르면 없애버리자..
-# placeholder = st.empty()
+# with col1:
+#     if st.button("객관식"):
+#         st.session_state.selected_type = 1
+# with col2:
+#     if st.button("주관식"):
+#         st.session_state.selected_type = 2
+# with col3:
+#     if st.button("서술형"):
+#         st.session_state.selected_type = 3
+    
+# if st.session_state.selected_type == 1:
+#     options = st.multiselect(
+#             "객관식의 유형을 선택해주세요 (선택)",
+#             ["하나만 고르기", "여러개 고르기"],
+#             ["하나만 고르기", "여러개 고르기"])
+# elif st.session_state.selected_type == 2:
+#     st.write("주관식 선택")
+# elif st.session_state.selected_type == 3:
+#     st.write("서술형 선택")
 
-# # Replace the placeholder with some text:
-# placeholder.text("Hello")
+# uploaded_file = st.file_uploader("문제를 낼 학습자료를 업로드 해주세요")
+# if uploaded_file is not None:
+#     st.write("uploaded!")
+    
+# if st.button("문제 생성하기"):
+#     prompt = ChatPromptTemplate.from_template("tell me a short joke about {topic}")
+#     output_parser = StrOutputParser()
 
-# # Replace the text with a chart:
-# placeholder.line_chart({"data": [1, 5, 2, 6]})
+#     chain = prompt | model | output_parser
 
-# # Replace the chart with several elements:
-# with placeholder.container():
-#     st.write("This is one element")
-#     st.write("This is another")
-
-# # Clear all those elements:
-# placeholder.empty()
+#     prompt_value = chain.invoke({"topic": "ice cream"})
+#     print(prompt_value)
+#     st.write("hello world!")
+#     st.write(prompt_value)
